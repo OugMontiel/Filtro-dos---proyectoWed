f2ee82f1a33a09a2b3e0b3aab77252ca388c3343
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-05-17 11:24:03 -0500

    feat: :art: un poco de stilo css
    a ver que sale :v primero hagamos todo ene css bien y salimos ay si hacerlo js

--- src/js/litOne.js
+++ src/js/litOne.js
@@ -4,7 +4,57 @@ export class productos extends LitElement {
     constructor() {
         super()
     }
-    static styles = css`~`;
+    static styles = css`
+    .producto {
+        /* max-height: 50%; */
+        /* max-width: 50%; */
+    
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    
+        /* overflow: hidden; */
+    
+        border: 1px solid var(--color-producto);
+        background: var(--color-producto);
+        border-radius: 1em;
+    }
+    
+    .producto img {
+        padding: .5em;
+        max-height: 70%;
+        max-width: 100%;
+    }
+    
+    .producto div {
+        flex-grow: 1;
+    
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    
+        border: 2px solid var(--color-Boton);
+        border-radius: 1em;
+        padding: 0.5em;
+    
+        background: var(--color-fondo);
+        color: var(--color-Boton);
+    }
+    
+    .producto div span {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    button {
+        height: 2em;
+        width: 7em;
+    
+        border-radius: .5em;
+        background: var(--color-Boton);
+        color: var(--color-Boton-letras);
+    }
+    `;
 
     render() {
         return html`
@@ -28,7 +78,8 @@ export class Barra extends LitElement {
     constructor() {
         super()
     }
-    static styles = css`~`;
+    static styles = css`
+    `;
 
     render() {
         return html`
