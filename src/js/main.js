d2cfbbcde30f1ed5618036b599f9da8cc4c1afa6
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-05-17 21:39:36 -0500

    feat: :ambulance: antes del error :C
    se daño el componente

--- src/js/main.js
+++ src/js/main.js
@@ -12,11 +12,11 @@ import {
 
 customElements.define("barra-dos", Barra);
 customElements.define("productos-disponibles", productos);
-getCarrito().then(data => {
-    console.log('getCarrito result:', data);
-}).catch(error => {
-    console.error('Error fetching data:', error);
-});
+// getCarrito().then(data => {
+//     console.log('getCarrito result:', data);
+// }).catch(error => {
+//     console.error('Error fetching data:', error);
+// });
 
 function Desplegable() {
     var btn = document.getElementById("Menu");
